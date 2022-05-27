de6038ca89f9fae8e0a2550aed5918c0f1d12086
Choiji92 <jeahoon100@naver.com>
2022-05-27 18:57:23 +0900

    키워드 - first commit
    변경사항 :

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Home from "./Home";
+import WordAdd from "./WordAdd";
+import WordUpdate from "./WordUpdate";
+import styled from "styled-components";
+import { Route, Switch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { loadWordFB } from "./redux/modules/word";
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect( async ()=>{
+      await dispatch(loadWordFB())
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>영어 단어장</Container>
+      <Switch>
+        <Route path= '/' exact>
+          <Home />
+        </Route>
+        <Route path="/word/add" exact>
+          <WordAdd/>
+        </Route>
+        <Route path="/word/:id/edit" exact>
+          <WordUpdate/>
+        </Route>
+      </Switch>
     </div>
   );
 }
+
+const Container = styled.div`
+  border-bottom: 2px solid #CCE5FF;
+  color: black;
+  font-size: x-large;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+`;
 
 export default App;
